efdea22ff0439a12746138f4ea2dace2b259cdc3
Tiffany Rodrigo <tiffa.rodrigs@gmail.com>
2022-04-18 22:25:48 -0700

    Add business logic

--- js-jest/src/gilded_rose.js
+++ js-jest/src/gilded_rose.js
@@ -10,7 +10,57 @@ class Shop {
   constructor(items=[]){
     this.items = items;
   }
-  updateQuality() {
+
+  updateQuality1() {
+    
+    // -Once the sell by date has passed, Quality degrades twice as fast
+    // -The Quality of an item is never negative
+	  // - The Quality of an item is never more than 50
+	  // - "Aged Brie" actually increases in Quality the older it gets --- covered
+	  // - "Sulfuras", being a legendary item, never has to be sold or decreases in Quality  --- covered
+	  // - "Backstage passes", like aged brie, increases in Quality as its SellIn value approaches; --- covered
+	  // Quality increases by 2 when there are 10 days or less and by 3 when there are 5 days or less but
+	  // Quality drops to 0 after the concert
+
+    this.items = this.items.map((item) => {
+      if(item.name == "Sulfuras, Hand of Ragnaros") {
+        return item;
+      }
+
+      
+      item.sellIn = item.sellIn - 1;
+      if(item.name=="Aged Brie" && item.quality <= 50 ) {
+        item.quality = item.quality + 1;
+      }
+      if(item.sellIn < 0) {
+        if(item.quality >= 2) {
+        item.quality = item.quality - 2
+        } else {
+          item.quality = 0;
+        }
+      }
+      if(item.name == "Backstage passes to a TAFKAL80ETC concert"){
+        if(item.sellIn > 0){
+          item.quality = item.quality + 1;
+          if(item.name == "Backstage passes to a TAFKAL80ETC concert" && item.sellIn <= 10){
+            item.quality = ietm.quality + 1;
+            if(item.sellIn <= 5){
+            item.quality = ietm.quality + 1;
+            }
+          }
+        } else {
+          item.quality = 0;
+        }
+      } else if(item.name == "Aged Brie"){
+
+      }
+      return item;
+    });
+
+    return this.items;
+  }
+
+  /*updateQuality() {
     for (let i = 0; i < this.items.length; i++) {
       if (this.items[i].name != 'Aged Brie' && this.items[i].name != 'Backstage passes to a TAFKAL80ETC concert') {
         if (this.items[i].quality > 0) {
@@ -58,7 +108,7 @@ class Shop {
     }
 
     return this.items;
-  }
+  }*/
 }
 
 module.exports = {
